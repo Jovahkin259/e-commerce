e3e3a7335a528951914c7257cff60b17d4e5a6bc
Joshua Astle <jovahkin259@gmail.com>
2021-06-12 14:35:33 +0100

    Connect the app and database

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const errorHandler = require('errorhandler')
 const morgan = require('morgan')
 const app = express()
 const PORT = process.env.PORT || 3000
+const db = require('./db/index')
 
 app.use(bodyParser.json())
 app.use(errorHandler())
 app.use(morgan('dev'))
 
-app.get('/', (req, res) => res.json({ info: 'Node.js, Express, and Postgres' }))
+app.get('/', async (req, res) => {
+  const { rows } = await db.query('SELECT * FROM users;')
+  res.send(rows[0])
+})
 
 app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`))
