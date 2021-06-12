b0d58180de49ddeb7529910f77bc8939acbadbad
Joshua Astle <jovahkin259@gmail.com>
2021-06-12 20:15:43 +0100

    refactor registration

--- routes/register.js
+++ routes/register.js
@@ -1,19 +1,25 @@
 const registerRouter = require('express-promise-router')()
 const db = require('../db/index')
 
-// example query
-// localhost:3000/register?username="dolly"&password="test123"&first_name="amellia"&last_name="brown"&phone=123
+const validateRegistration = (req, res, next) => {
+  if (!req.query.username || !req.query.password || !req.query.first_name || !req.query.last_name) {
+    res.status(400).send()
+  } else {
+    req.values = [
+      req.query.username,
+      req.query.password,
+      req.query.first_name,
+      req.query.last_name,
+      req.query.phone ? req.query.phone : null
+    ]
+    next()
+  }
+}
+registerRouter.post('/', validateRegistration)
+
 registerRouter.post('/', async (req, res, next) => {
-  const query = 'INSERT INTO users(username, password, first_name, last_name, phone) VALUES($1, $2, $3, $4, $5) RETURNING *'
-  const values = [
-    req.query.username,
-    req.query.password,
-    req.query.first_name,
-    req.query.last_name,
-    req.query.phone ? req.query.phone : null
-  ]
   try {
-    const result = await db.query(query, values)
+    const result = await db.query(req.sql = 'INSERT INTO users(username, password, first_name, last_name, phone) VALUES($1, $2, $3, $4, $5) RETURNING *', req.values)
     res.status(201).json(result.rows[0])
   } catch (err) {
     next(err)
