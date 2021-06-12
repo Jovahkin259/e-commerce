2e88ab0287d4b0f076a9d0c78acb6a15b8b46e81
Joshua Astle <jovahkin259@gmail.com>
2021-06-12 12:31:38 +0100

    Initialized server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const errorHandler = require('errorhandler')
+const morgan = require('morgan')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => res.send('Hello world'))
+
+app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`))
